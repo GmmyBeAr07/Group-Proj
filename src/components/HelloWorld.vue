cae8005aa032875c7b16eef2b67c392dd5a52759
GmmyBeAr07 <dwwildfire@gmail.com>
2022-02-28 09:10:37 +0200

    yess..

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <section id="Home" style="min-height: 100vh; justify-content: center; flex-direction: column; display: flex;">
+        
+        <div class="w3-container w3-center w3-animate-top">
+<span class="h1"><h1>Welcome To </h1></span>
+<span class="h2"><h2>Hire Your Own Developer</h2></span>
+<br>
+<span class="h3"><h3><button>Hire Us</button></h3></span>
+</div> 
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+  
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style scoped>
+section {
+    background:linear-gradient(0deg, rgba(0, 0, 0, 0.281), rgba(56, 10, 10, 0.877)), url('../assets/images/code.jpg');
+    background-repeat: no-repeat;
+    background-attachment: fixed; 
+    backface-visibility:visible;
+    background-size: 100% 100%;
+  }
+
+#form-control {
+ min-width: 40%;
+}
+</style>
